78fe558bbc3cf1717ab8c13878616a96372179e5
Vontk <121203087+Vontk@users.noreply.github.com>
2024-04-19 05:55:51 -0700

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    año=int(input("Ingrese un año:"))
+    if (año//4)==(año/4) or (año/100)==(año//100) and (año/400)==(año//400):
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
